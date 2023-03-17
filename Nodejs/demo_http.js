a6494d0f84d974782c57ede2cc65ad14cc51ef27
Chiptune <phamtienphat123@gmail.com>
2023-03-16 19:18:26 -0700

    Finished js basic part 2 and Demo Nodejs

---
+++ Nodejs/demo_http.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (req, res){
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  res.write('Hello World!');
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+  res.end(txt);
+
+  res.end();
+}).listen(8080);
