e7a6d12d972c82984cbe9b7412af7bd826482da7
Chiptune <phamtienphat123@gmail.com>
2023-03-25 00:26:19 -0700

    Add // to url part to define protocol

--- Nodejs/Basic-Information-Site/index.js
+++ Nodejs/Basic-Information-Site/index.js
@@ -1,37 +1,39 @@
-let http = require('http');
-let fs = require('fs');
+var http=require('http');
+var fs=require('fs');
 
-http.createServer(function(req,res){
-  if(req.url === '/'){
-    fs.readFile('index.html',function(err,data){
-      if(err) throw err;
-      res.writeHead(200,{'Content-type':'text,html'});
-      res.write(data);
-      return res.end;
-    });
-  }
-  else if(req.url ==='/about'){
-    fs.readFile('about.html',function(err,data){
-      if(err) throw err;
-      res.writeHead(200,{'Content-type':'text,html'});
-      res.write(data);
-      return res.end;
-    })
-  }
-  else if(req.url === "/contact-me"){
-    fs.readFile('contact-me.html',function(err,data){
-      if(err) throw err;
-      res.writeHead(200,{'Content-type':'text,html'});
-      res.write(data);
-      return res.end;
-    })
-  }
-  else {
-    fs.readFile("404.html",function(err,data){
-      if(err) throw err;
-      res.writeHead(404,{'Content-type':'text,html'});
-      res.write(data);
-      return res.end;
-    })
-  }
-}).listen(8080);
+http.createServer((req,res)=>{
+    if(req.url==='/'){
+        fs.readFile('index.html',function(err,data){
+            if (err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end;
+        });
+    }
+    else if(req.url === '/about'){
+        fs.readFile('about.html',function(err,data){
+            if (err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end;
+        });
+    }
+    else if(req.url==='/contact-me'){
+        fs.readFile('contact-me.html',function(err,data){
+            if (err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end;
+        });
+    }
+    else{
+        fs.readFile('404.html',function(err,data){
+            if (err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            return res.end;
+        });
+    }
+}).listen(8080,()=>{
+    console.log('Listening on port  8080.....');
+});
