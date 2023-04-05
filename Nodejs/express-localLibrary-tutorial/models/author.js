707007a93944280d1ebf96a888da5ac7d524ac2e
Chiptune <phamtienphat123@gmail.com>
2023-04-05 14:13:26 -0700

    Add author model

---
+++ Nodejs/express-localLibrary-tutorial/models/author.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 100 },
+  family_name: { type: String, required: true, maxLength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date },
+});
+
+//Virtual to get author's full name
+AuthorSchema.virtual("getFullName").get(function () {
+  let fullname = "";
+  // if author doesn't has either first name or family name will return an empty string
+  if (this.first_name && this.family_name) {
+    fullname = `${this.first_name}, ${this.family_name}`;
+  }
+  if (!this.first_name || !this.family_name) {
+    fullname = "";
+  }
+  return fullname;
+});
+
+//Virtual to return the url of specific author
+AuthorSchema.virtual("getAuthorUrl").get(function () {
+  return `/catalog/author/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("Author", AuthorSchema);
