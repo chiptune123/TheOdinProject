dc04cfbb96b52753ddce7b7cdddfec3ba69cfd8c
Chiptune <phamtienphat123@gmail.com>
2023-04-06 15:45:01 -0700

    Add genre modal

---
+++ Nodejs/express-localLibrary-tutorial/models/genre.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const GenreSchema = new Schema ({
+  name: {type: String, required: true, minLength: 3, maxLength:100},
+})
+
+GenreSchema.virtual("GetBookUrl").get(function(){
+  return `/catalog/genre/${this._id}`
+})
