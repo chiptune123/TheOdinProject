5f37f16b357d044017d2b4b887197d99bdab5656
Chiptune <phamtienphat123@gmail.com>
2023-04-18 22:15:52 -0700

    Add async Handler middleware to genreController

--- Nodejs/express-localLibrary-tutorial/controllers/genreController.js
+++ Nodejs/express-localLibrary-tutorial/controllers/genreController.js
@@ -1,42 +1,42 @@
 const Genre = require("../models/genre");
+const asyncHandler = require("express-async-handler");
 
 //Display list of all Genre.
-exports.genre_list = (req,res) =>{
+exports.genre_list =  asyncHandler(async (req,res,next) => {
   res.send("NOT IMPLEMENTED: Genre list");
-}
+});
 
 //Display detail page for specific genre.
-exports.genre_detail = (req,res) =>{
-  res.send(`NOT IMPLEMENTED: Genre detail: ${req.param.id}`);
-}
+exports.genre_detail =  asyncHandler(async (req,res,next) => {
+  res.send(`NOT IMPLEMENTED: Genre detail: ${req.params.id}`);
+});
 
 //Display create Genre form on GET.
-exports.genre_create_get = (req,res) => {
+exports.genre_create_get =  asyncHandler(async (req,res,next) => {
   res.send("NOT IMPLEMENTED: Genre create GET");
-}
+});
 
 //Handle create Genre on POST
-exports.genre_create_post = (req,res) => {
+exports.genre_create_post =  asyncHandler(async (req,res,next) => {
   res.send("NOT IMPLEMENTED: Genre create POST");
-}
+});
 
 //Display delete Genre form on GET.
-exports.genre_delete_get = (req,res) => {
+exports.genre_delete_get =  asyncHandler(async (req,res,next) => {
   res.send("NOT IMPLEMENTED: Genre delete GET");
-}
+});
 
 //Handle delete Genre on POST.
-exports.genre_delete_post = (req,res) =>{
+exports.genre_delete_post =  asyncHandler(async (req,res,next) => {
   res.send("NOT IMPLEMENTED: Genre delete POST");
-}
+});
 
 //Display update Genre form on GET.
-exports.genre_update_get = (req,res) => {
+exports.genre_update_get =  asyncHandler(async (req,res,next) => {
   res.send("NOT IMPLEMENTED: Genre update GET");
-}
+});
 
 //Handle update Genre on POST
-exports.genre_update_post = (req,res) => {
+exports.genre_update_post =  asyncHandler(async (req,res,next) => {
   res.send("NOT IMPLEMENTED: Genre update POST");
-}
- 
+});
