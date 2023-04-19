ac6d15d3da1c86694c886fd49e0405b2b76315e3
Chiptune <phamtienphat123@gmail.com>
2023-04-18 22:09:11 -0700

    Add Async-Handler middleware to handle exception in authorController

--- Nodejs/express-localLibrary-tutorial/controllers/authorController.js
+++ Nodejs/express-localLibrary-tutorial/controllers/authorController.js
@@ -1,41 +1,43 @@
 const Author = require("../models/author");
+const asyncHandler = require("express-async-handler");
+//This middleware using to caught exception and passing to express error handler
 
 //Display list of all Authors.
-exports.author_list = (req, res) => {
+exports.author_list = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Author list");
-};
+});
 
 //Display detail page for a specific Author
-exports.author_detail = (req, res) => {
+exports.author_detail = asyncHandler(async (req, res, next) => {
   res.send(`NOT IMPLEMENTED: Author detail: ${req.params.id}`);
-};
+});
 
 //Display Author create form on GET.
-exports.author_create_get = (req, res) => {
+exports.author_create_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Author create GET");
-};
+});
 
 //Handle author create on POST.
-exports.author_create_post = (req, res) => {
+exports.author_create_post = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Author create POST");
-};
+});
 
 //Display author delete form on GET.
-exports.author_delete_get = (req, res) => {
+exports.author_delete_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Author delete GET");
-};
+});
 
 //Handle Author delete on POST.
-exports.author_delete_post = (req, res) => {
+exports.author_delete_post = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Author delete POST");
-};
+});
 
 //Display Author update form on GET.
-exports.author_update_get = (req, res) => {
+exports.author_update_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Author update GET");
-};
+});
 
 //Handle author update on POST.
-exports.author_update_post = (req, res) => {
+exports.author_update_post = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Author delete post");
-};
+});
