7fd00df0818e52597a61f9c9f22e979ba13bcaf3
Chiptune <phamtienphat123@gmail.com>
2023-04-18 22:37:15 -0700

    Add async Handler to bookController

--- Nodejs/express-localLibrary-tutorial/controllers/bookController.js
+++ Nodejs/express-localLibrary-tutorial/controllers/bookController.js
@@ -3,80 +3,83 @@ const Author = require("../models/author");
 const BookInstance = require("../models/bookinstance");
 const Genre = require("../models/genre");
 
+const asyncHandler = require("express-async-handler");
 const async = require("async");
 const bookinstance = require("../models/bookinstance");
 const genre = require("../models/genre");
 
 //Display welcome page
-exports.index = (req, res) => {
-  async.parallel(
-    {
-      book_count(callback) {
-        //Call back as parameter will save the result of function to this object
-        Book.countDocuments({}, callback); //the "callback" is the result of query and this function will return to book_count object
-      },
-      book_instance_count(callback) {
-        BookInstance.countDocuments({}, callback);
-      },
-      book_instance_available_count(callback) {
-        BookInstance.countDocuments({ status: "Available" }, callback);
-      },
-      author_count(callback) {
-        Author.countDocuments({}, callback);
-      },
-      genre_count(callback) {
-        Genre.countDocuments({}, callback);
-      },
-    },
-    //the async.parallel has two parameters. The first parameter we put objects, the second parameter
-    //we put call back which using first-error convention, which mean the results will contain all 
-    //the object from the first parameter of async.parallel.
-    (err, results) => { 
-      res.render("index", {
-        title: "Local Library Home",
-        error: err,
-        data: results,
-      })
-    }
-  );
-};
+// exports.index = asyncHandler(async (req, res, next) => {
+//   async.parallel(
+//     {
+//       book_count(callback) {
+//         //Call back as parameter will save the result of function to this object
+//         Book.countDocuments({}, callback); //the "callback" is the result of query and this function will return to book_count object
+//       },
+//       book_instance_count(callback) {
+//         BookInstance.countDocuments({}, callback);
+//       },
+//       book_instance_available_count(callback) {
+//         BookInstance.countDocuments({ status: "Available" }, callback);
+//       },
+//       author_count(callback) {
+//         Author.countDocuments({}, callback);
+//       },
+//       genre_count(callback) {
+//         Genre.countDocuments({}, callback);
+//       },
+//     },
+//     //the async.parallel has two parameters. The first parameter we put objects, the second parameter
+//     //we put call back which using first-error convention, which mean the results will contain all
+//     //the object from the first parameter of async.parallel.
+//     (err, results) => {
+//       res.render("index", {
+//         title: "Local Library Home",
+//         error: err,
+//         data: results,
+//       });
+//     }
+//   );
+// });
+//This is old code on MDN and they put new update on April 15, 2023
+
 
 //Display list of all books.
-exports.book_list = (req, res) => {
+exports.book_list = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Book list");
-};
+});
 
 //Display detail page for a specific book
-exports.book_detail = (req, res) => {
+exports.book_detail = asyncHandler(async (req, res, next) => {
   res.send(`NOT IMPLEMENTED: Book detail: ${req.params.index}`);
-};
+});
 
 //Display book create form on GET.
-exports.book_create_get = (req, res) => {
+exports.book_create_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Book create GET");
-};
+});
 
 //Handle book create on POST.
-exports.book_create_post = (req, res) => {
+exports.book_create_post = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Book create POST");
-};
+});
 
 //Display book delete form on GET.
-exports.book_delete_get = (req, res) => {
+exports.book_delete_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Book delete GET");
-};
+});
 
 //Display book delete form on POST.
-exports.book_delete_post = (req, res) => {
+exports.book_delete_post = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Book delete POST");
-};
+});
 
 //Display book update form on GET.
-exports.book_update_get = (req, res) => {
+exports.book_update_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Book update GET");
-};
+});
 
 //Display book update form on POST.
-exports.book_update_post = (req, res) => {
+exports.book_update_post = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Book create POST");
-};
+});
