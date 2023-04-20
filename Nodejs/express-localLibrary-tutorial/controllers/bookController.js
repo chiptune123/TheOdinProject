e822cb236c2e9dd8c658a0dd393ce750d8818f85
Chiptune <phamtienphat123@gmail.com>
2023-04-19 20:01:16 -0700

    Add async handler to handle exception and update new way to query data according to MDN

--- Nodejs/express-localLibrary-tutorial/controllers/bookController.js
+++ Nodejs/express-localLibrary-tutorial/controllers/bookController.js
@@ -7,6 +7,7 @@ const asyncHandler = require("express-async-handler");
 const async = require("async");
 const bookinstance = require("../models/bookinstance");
 const genre = require("../models/genre");
+const { exec } = require("selenium-webdriver/io/exec");
 
 //Display welcome page
 // exports.index = asyncHandler(async (req, res, next) => {
@@ -43,6 +44,30 @@ const genre = require("../models/genre");
 // });
 //This is old code on MDN and they put new update on April 15, 2023
 
+exports.index = asyncHandler(async (req, res, next) => {
+  // Get details of books, book instances, authors and genre counts (in parallel)
+  const [
+    numBooks,
+    numBookInstances,
+    numAvailableBookInstances,
+    numAuthors,
+    numGenres,
+  ] = await Promise.all([
+    Book.countDocuments({}).exec(),
+    BookInstance.countDocuments({}).exec(),
+    BookInstance.countDocuments({status: "Available"}).exec(),
+    Author.countDocuments({}).exec(),
+    Genre.countDocuments({}).exec(),
+  ]);
+  res.render("index", {
+    title: "Local Library Home",
+    book_count: numBooks,
+    book_instance_count: numBookInstances,
+    book_instance_available_count: numAvailableBookInstances,
+    author_count: numAuthors,
+    genre_count: numGenres,
+  });
+});
 
 //Display list of all books.
 exports.book_list = asyncHandler(async (req, res, next) => {
