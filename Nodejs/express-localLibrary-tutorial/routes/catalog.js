05e77813b9abeb46ae030c5b40fb6df1d9443c6c
Chiptune <phamtienphat123@gmail.com>
2023-04-20 18:37:54 -0700

    Fix error in url path of route handler authors and error in authors hyperlink name in layout.ejs

--- Nodejs/express-localLibrary-tutorial/routes/catalog.js
+++ Nodejs/express-localLibrary-tutorial/routes/catalog.js
@@ -59,8 +59,8 @@ router.post("/author/:id/update", author_controller.author_update_post);
 // GET request for display an author
 router.get("/author/:id", author_controller.author_detail);
 
-// POST request for display list of all authors
-router.post("/authors", author_controller.author_list);
+// GET request for display list of all authors
+router.get("/authors", author_controller.author_list);
 
 // GENRE ROUTES //
 
