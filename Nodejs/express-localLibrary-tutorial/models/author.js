b02fb3ccd47b5c44a410872caa2c3e7ec4bf1f46
Chiptune <phamtienphat123@gmail.com>
2023-04-26 01:15:41 -0700

    Edit the getFullName virtual to exchange family name and first name position

--- Nodejs/express-localLibrary-tutorial/models/author.js
+++ Nodejs/express-localLibrary-tutorial/models/author.js
@@ -15,7 +15,7 @@ AuthorSchema.virtual("getFullName").get(function () {
   let fullname = "";
   // if author doesn't has either first name or family name will return an empty string
   if (this.first_name && this.family_name) {
-    fullname = `${this.first_name}, ${this.family_name}`;
+    fullname = `${this.family_name}, ${this.first_name}`;
   }
   if (!this.first_name || !this.family_name) {
     fullname = "";
